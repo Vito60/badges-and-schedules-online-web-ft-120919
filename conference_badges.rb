7c4be8b0c1ff02cd14471dc82857a342a27cea27
Randall Williams <randallw1996@gmail.com>
2019-12-11 17:25:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,53 @@
-# Write your code here.
+#require 'pry'
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+  
+
+
+def batch_badge_creator(array)
+  badges = []
+  array.each do |attendee|
+    badges << badge_maker(attendee)
+  end
+  badges
+
+end
+
+def assign_rooms(array)
+  rooms = []
+  counter = 1 
+  array.each do |name|
+    rooms << "Hello, #{name}! You'll be assigned to room #{counter}!"
+  counter += 1 
+  end
+  rooms 
+end
+
+def printer(attendees)
+  new_array = []
+  new_array << batch_badge_creator(attendees)
+  new_array.each do |name|
+    puts name
+  end
+end
+
+  
+  
+  
+  
+  # batch_badge_creator(attendees)
+  #   index = 0 
+  #   while index < badges.length do 
+  #     puts badges[index]
+  #     index += 1 
+  #   end
+
+# end
+
+
+
+
+
+
